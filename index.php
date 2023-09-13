2177ce2c1ff72642a4065062dad059e70ede8fac
leoneinfinito <samuelepelella22@gmail.com>
2023-09-13 02:08:12 +0200

    TROLL

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MANDA A FANCULO CHI VUI TU!</title>
+</head>
+<body>
+    <img src="https://cdn.pixabay.com/photo/2018/01/15/23/52/smile-3085057_1280.png" alt="Una grande immagine di un sorriso" style="display: block; margin: 0 auto; width: 600px;">
+    <h1 style="color: red; text-align: center; font-size: 40px;">SEI STATO APPENA MANDATO A FANCULO</h1>
+    <p style="text-align: center; font-size: 25px;">
+        Vuoi vendicarti?
+        <a href="https://www.youtube.com/watch?v=oHg5SJYRHA0" style="text-decoration: none;">
+            <button style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; cursor: pointer; font-size: 30px;">PERFETTO CLICCA QUI!</button>
+        </a>
+    </p>
+</body>
+</html>
